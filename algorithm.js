cb55eb62151b3e51b3c7875883ad426fb444d145
IDDER Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2023-11-06 04:58:11 +0100

    Create first version of Etch-a-Sketch project
    This is the first version of the Etch-a-Sketch project from the Odin curriculum. It focuses on the logic behind the sketching functionality. The UI is not yet styled and will be improved later.

---
+++ algorithm.js
@@ -0,0 +1,63 @@
+const table = document.querySelector('#table');
+
+let gridDivEl = [];
+
+
+
+
+for(let i=0 ; i<256 ; i++){
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    table.appendChild(div);
+    div.style.flex = `1 0 6.25%`;
+    //push to the array
+    gridDivEl.push(div);
+}
+
+gridDivEl.forEach(div => div.addEventListener('mouseover', function(){
+    div.style.backgroundColor ='white';
+}))
+
+function gridSetUp(row , column){
+    table.textContent='';
+    let numOfDivs = row * column ;
+    let widthOfDivs = 100/row;
+
+    let gridDivEl = [];
+
+    for(let i=0 ; i<numOfDivs ; i++){
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        table.appendChild(div);
+        div.style.flex = `1 0 ${widthOfDivs}%`;
+        //push to the array
+        gridDivEl.push(div);
+    }
+    gridDivEl.forEach(div => div.addEventListener('mouseover', function(){
+        div.style.backgroundColor ='white';
+    }))
+}
+
+
+const GridSet = document.querySelector('.gridSet');
+
+GridSet.addEventListener('click', () =>{
+    // Prompt the user for the number of squares in a row
+    let gridRow = prompt('Please enter the number of squares you want in a row:');
+
+    // Prompt the user for the number of squares in a column
+    let gridColumn = prompt('Please enter the number of squares you want in a column:');
+
+    gridRow = Number(gridRow);
+    gridColumn = Number(gridColumn);
+
+   // Check if the inputs are valid numbers and within the specified range
+    if (isNaN(gridRow) || isNaN(gridColumn) || gridRow > 100 || gridColumn > 100) {
+        alert('Invalid input. Please enter a number less than or equal to 100 for both row and column.');
+    } else {
+        console.log(`You have entered ${gridRow} squares for the row and ${gridColumn} squares for the column.`);
+        gridSetUp(gridRow, gridColumn)
+
+    }
+
+})
